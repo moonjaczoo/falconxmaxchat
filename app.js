51f390371a46d69823e544d50dc1802123129bb8
unknown <somnabomna@gmail.com>
2021-05-06 18:23:34 +0200

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var alert = require('alert');
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
