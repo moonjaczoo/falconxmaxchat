55ccfd0fb81a82c4d322c8507572396a96720003
unknown <somnabomna@gmail.com>
2021-05-06 18:38:39 +0200

    make it better

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var http = require('http');
 
 var ports = process.env.PORT || 8080;
-
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 http.listen(ports, function () {
     
