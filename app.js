3389a1f69fffbfe62120d1593efd51bb2ee605a3
unknown <somnabomna@gmail.com>
2021-05-06 18:43:04 +0200

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var http = require('http');
+var http = require('http').Server(app);
+
 
 var ports = process.env.PORT || 8080;
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(ports, function () {
-    
-    console.log('Node app is working!');
+
+
+
+http.listen(8080, function(){
+console.log('💥 YOUR WEBSOCKET IS RUNING 💥');
 });
