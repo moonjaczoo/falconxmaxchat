b51f8951959a37313343de2283b9181a4fdeec79
unknown <somnabomna@gmail.com>
2021-05-06 19:43:00 +0200

    make it better

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 4000, function () {
+app.listen(process.env.PORT || 8080, function () {
     console.log('Node app is working!');
 });
