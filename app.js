628c3c5107d9806270f22ad86c476d2622840659
unknown <somnabomna@gmail.com>
2021-05-06 19:06:15 +0200

    make it better

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
-var ports = process.env.PORT || 8080;
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+				io.on('connection', function(socket){
+				console.log('NEW USER CONNECTED !');
+				var myroom = socket.handshake.query.uid;
+				console.log(myroom);
+				socket.join(myroom);
+
+
+				socket.on('disconnect', () => {
+				console.log('USER DISCONNECTED !');
+				socket.leave(myroom);
+				})
+				socket.on('msg', function(data) {
+				console.log("Success______________________. ✔");
+				io.sockets.in(myroom).emit('msg',{message: data.message,room: data.room,name :data.name});	    			    		
+				});
+				});
+
+
 
 
 
