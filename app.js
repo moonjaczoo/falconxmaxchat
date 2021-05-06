0985ac1ed507deafa441c965ad3093edd73b5a63
unknown <somnabomna@gmail.com>
2021-05-06 19:53:54 +0200

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080, function () {
-    console.log('Node app is working!');
-});
+const cheerio = require('cheerio')
+const $ = cheerio.load(`
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <p id='iwwinm'>I want to be stored in a node.js var? How?!</p>
+  </body>
+</html>
+`);
+$('#iwwinm').text('hello world');
+console.log($('#iwwinm'));
