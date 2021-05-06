113ed56a77dc992ef924add6a62c3ed2c4b382cd
unknown <somnabomna@gmail.com>
2021-05-06 18:33:41 +0200

    make it better

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 var path = require('path');
 var http = require('http');
 
-var port = process.env.PORT || 8080;
+var ports = process.env.PORT || 8080;
 
 
-http.listen(port, function () {
+http.listen(ports, function () {
     
     console.log('Node app is working!');
 });
