6be939e35802ebbcb3c57f739d5b21fe689b7251
unknown <somnabomna@gmail.com>
2021-05-06 19:20:39 +0200

    make it better

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var path = require('path');
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-				io.on('connection', function(socket){
-				console.log('NEW USER CONNECTED !');
-				var myroom = socket.handshake.query.uid;
-				console.log(myroom);
-				socket.join(myroom);
-
-
-				socket.on('disconnect', () => {
-				console.log('USER DISCONNECTED !');
-				socket.leave(myroom);
-				})
-				socket.on('msg', function(data) {
-				console.log("Success______________________. ✔");
-				io.sockets.in(myroom).emit('msg',{message: data.message,room: data.room,name :data.name});	    			    		
-				});
-				});
-
-
-
-app.listen(8080, function(){
-console.log('💥 YOUR WEBSOCKET IS RUNING 💥');
+app.listen(process.env.PORT || 4000, function () {
+    console.log('Node app is working!');
 });
