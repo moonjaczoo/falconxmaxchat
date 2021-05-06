24b8c84aa5e5d670cf2e568c6bcf6d6bd76ee50c
unknown <somnabomna@gmail.com>
2021-05-06 18:27:24 +0200

    make it better

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var alert = require('alert');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-alert("dsadas");
+
+
+
 app.listen(process.env.PORT || 8080, function () {
     
     console.log('Node app is working!');
