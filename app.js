f8acd408a01dcf303804edd39bd33c6489588ea9
unknown <somnabomna@gmail.com>
2021-05-08 00:31:41 +0200

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const cheerio = require('cheerio')
-const $ = cheerio.load(`
-<!DOCTYPE html>
-<html>
-  <head></head>
-  <body>
-    <p id='iwwinm'>I want to be stored in a node.js var? How?!</p>
-  </body>
-</html>
-`);
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 1111
 
+app.use(express.static(__dirname + "/"));
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
+
+wss.on("connection", function(ws) {
+ console.log("websocket connection open");
+  ws.on("message", function(msg) {
+    console.log(msg);
+  });
+  ws.on("close", function() {
+    console.log("websocket connection close");
+  });
+});
