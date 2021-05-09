9ccf8b636f89ff9fa995a3c6020e95505c5835df
unknown <somnabomna@gmail.com>
2021-05-09 14:58:46 +0200

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 app.get('/', function (req, res) {
@@ -19,14 +15,6 @@ app.get('/', function (req, res) {
   res.render('index.html', { pageCountMessage : null});
 });
 
-
-/*
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-*/
 app.listen(port, ip);
 console.log('Server running on dsdahttp://%s:%s', ip, port);
 
