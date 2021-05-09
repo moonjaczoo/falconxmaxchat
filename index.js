415865ee1b3f4afff2070ce0517e2f57eceedf60
unknown <somnabomna@gmail.com>
2021-05-09 13:47:21 +0200

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var http = require("http");
 
-app.use(express.static(__dirname + "/"))
+const server = http.createServer(
+    (req ,res)=>{
+        res.writeHead({
+            "content-type":"text/html"
+        });
+        res.end("<h1>hello world </h1>");
+    }
+);
 
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+server.listen(8000,function(){
+    console.log("from server");
+});
